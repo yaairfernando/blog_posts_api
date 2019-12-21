9d3262cf23db9bef9d96286e0680413c69543fdd
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-21 11:01:23 -0600

    Refactor the UserHeader component

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,7 +8,7 @@ class UserHeader extends React.Component {
   }
 
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
 
     if (!user) {
       return null;
@@ -20,8 +20,8 @@ class UserHeader extends React.Component {
   };
 };
 
-const mapStateToProps = (state) => {
-  return { users: state.users }
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) }
 }
 
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
