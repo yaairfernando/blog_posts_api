709d47b85201a38f284e5b1ebdb3045965c1cd56
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2019-12-21 10:52:10 -0600

    Displaying the author of each post on the UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
-export default fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
